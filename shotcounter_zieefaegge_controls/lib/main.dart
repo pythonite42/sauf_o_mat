2b151d75de0345d5bf4b73ae282bf3d2686444e9
sarahLoeffler <sarah.loeffler42@gmail.com>
2025-07-16 18:18:50 +0200

    add video recording

--- shotcounter_zieefaegge_controls/lib/main.dart
+++ shotcounter_zieefaegge_controls/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:shotcounter_zieefaegge_controls/colors.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,6 +35,60 @@ class _MyHomePageState extends State<MyHomePage> {
   int currentNavigationIndex = 0;
   List<String> pages = ["Balkendiagramm", "Top 3", "Gewinn", "Ablaufplan", "Kommentare", "Werbung", "Livestream"];
 
+  final _localRenderer = RTCVideoRenderer();
+  MediaStream? _localStream;
+  bool _isRecordingRunning = false;
+  bool _showCamera = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _localRenderer.initialize();
+  }
+
+  @override
+  void dispose() {
+    _localStream?.dispose();
+    _localRenderer.dispose();
+    super.dispose();
+  }
+
+  Future<void> _startCamera() async {
+    final Map<String, dynamic> mediaConstraints = {
+      'audio': false,
+      'video': {'facingMode': 'environment', 'width': 1280, 'height': 720, 'frameRate': 30},
+    };
+
+    try {
+      _localStream = await navigator.mediaDevices.getUserMedia(mediaConstraints);
+      _localRenderer.srcObject = _localStream;
+    } catch (e) {
+      print('Error getting user media: $e');
+    }
+  }
+
+  Future<void> _stopCamera() async {
+    try {
+      if (_localStream != null) {
+        // Stop all media tracks (both video and audio)
+        for (var track in _localStream!.getTracks()) {
+          track.stop();
+        }
+
+        // Release the stream
+        _localStream = null;
+      }
+
+      // Disconnect the stream from the renderer
+      _localRenderer.srcObject = null;
+
+      // Optionally trigger a rebuild if UI depends on camera state
+      setState(() {});
+    } catch (e) {
+      print('Error stopping camera: $e');
+    }
+  }
+
   Future<int> getCurrentNavigationIndex() async {
     await Future.delayed(Duration(seconds: 2));
     setState(() {
@@ -49,7 +104,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Scaffold(
       body: Padding(
-        padding: EdgeInsetsGeometry.only(top: 50),
+        padding: const EdgeInsets.only(top: 50),
         child: Column(
           children: <Widget>[
             FutureBuilder<int>(
@@ -59,22 +114,35 @@ class _MyHomePageState extends State<MyHomePage> {
                   return Row(
                     mainAxisAlignment: MainAxisAlignment.spaceAround,
                     children: [
-                      Text("Seite:", style: TextStyle(fontSize: 20)),
+                      const Text("Seite:", style: TextStyle(fontSize: 20)),
                       Container(
                         width: 200,
-                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        padding: const EdgeInsets.symmetric(horizontal: 10),
                         decoration: BoxDecoration(color: darkAccent),
-                        child: DropdownButtonFormField(
+                        child: DropdownButtonFormField<String>(
                           value: pages[snapshot.data!],
                           icon: const Icon(Icons.expand_more),
-                          decoration: InputDecoration(
+                          decoration: const InputDecoration(
                             enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.transparent)),
                           ),
                           onChanged: (String? newValue) async {
-                            await Future.delayed(Duration(seconds: 2));
+                            final newIndex = pages.indexOf(newValue!);
                             setState(() {
-                              currentNavigationIndex = 1;
+                              currentNavigationIndex = newIndex;
                             });
+
+                            if (newValue == "Livestream") {
+                              await _startCamera();
+                              setState(() {
+                                _showCamera = true;
+                              });
+                            } else {
+                              _localStream?.dispose();
+                              _localRenderer.srcObject = null;
+                              setState(() {
+                                _showCamera = false;
+                              });
+                            }
                           },
                           items: pages.map<DropdownMenuItem<String>>((String value) {
                             return DropdownMenuItem<String>(value: value, child: Text(value));
@@ -84,15 +152,15 @@ class _MyHomePageState extends State<MyHomePage> {
                     ],
                   );
                 } else {
-                  return Container();
+                  return const CircularProgressIndicator();
                 }
               },
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             LayoutBuilder(
               builder: (context, constraints) {
                 const double borderWidth = 1.0;
-                double totalInternalBorders = borderWidth * 3; // 1 border between 2 buttons + border left and right
+                double totalInternalBorders = borderWidth * 3;
                 double buttonWidth = (constraints.maxWidth - totalInternalBorders) / 2;
 
                 return ToggleButtons(
@@ -107,13 +175,13 @@ class _MyHomePageState extends State<MyHomePage> {
                   },
                   borderWidth: borderWidth,
                   borderRadius: BorderRadius.circular(8),
-                  color: Colors.white, // Unselected text/icon color
-                  selectedColor: Colors.white, // Selected text/icon color (for contrast)
-                  fillColor: darkAccent, // Selected background
-                  splashColor: transparentWhite, // Ripple effect on tap
-                  highlightColor: transparentWhite, // Pressed color effect
-                  borderColor: transparentWhite, // Border for unselected buttons
-                  selectedBorderColor: transparentWhite, // Border when selected
+                  color: Colors.white,
+                  selectedColor: Colors.white,
+                  fillColor: darkAccent,
+                  splashColor: transparentWhite,
+                  highlightColor: transparentWhite,
+                  borderColor: transparentWhite,
+                  selectedBorderColor: transparentWhite,
                   disabledColor: Colors.grey.shade600,
                   disabledBorderColor: Colors.grey.shade800,
                   children: [
@@ -123,8 +191,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Text(
                           "Ex-Cam",
                           style: TextStyle(
-                            fontSize: selected.first == true ? 20 : 16,
-                            fontWeight: selected.first == true ? FontWeight.bold : FontWeight.normal,
+                            fontSize: selected.first ? 20 : 16,
+                            fontWeight: selected.first ? FontWeight.bold : FontWeight.normal,
                           ),
                         ),
                       ),
@@ -135,8 +203,8 @@ class _MyHomePageState extends State<MyHomePage> {
                         child: Text(
                           "Kiss-Cam",
                           style: TextStyle(
-                            fontSize: selected.last == true ? 20 : 16,
-                            fontWeight: selected.last == true ? FontWeight.bold : FontWeight.normal,
+                            fontSize: selected.last ? 20 : 16,
+                            fontWeight: selected.last ? FontWeight.bold : FontWeight.normal,
                           ),
                         ),
                       ),
@@ -145,7 +213,50 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               },
             ),
-            Expanded(child: Center(child: Text('Kamera erscheint wenn Livestream ausgewählt ist'))),
+            SizedBox(height: 10),
+            Expanded(
+              child: _showCamera
+                  ? LayoutBuilder(
+                      builder: (context, constraints) {
+                        var controlBarHeight = constraints.maxHeight * 0.13;
+                        var height = constraints.maxHeight - controlBarHeight;
+                        var width = (constraints.maxHeight - controlBarHeight) * 9 / 16;
+                        return Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(width: width, height: height, child: RTCVideoView(_localRenderer, mirror: false)),
+                            Container(
+                              width: width,
+                              height: controlBarHeight,
+                              color: Colors.black,
+                              child: IconButton(
+                                onPressed: () async {
+                                  if (_isRecordingRunning) {
+                                    //stop sending data
+                                    //await _stopCamera();
+                                  } else {
+                                    //send data
+                                    //await _startCamera();
+                                  }
+
+                                  setState(() {
+                                    _isRecordingRunning = !_isRecordingRunning;
+                                  });
+                                },
+                                icon: Icon(
+                                  _isRecordingRunning ? Icons.stop_circle : Icons.play_circle,
+                                  color: _isRecordingRunning ? Colors.red : Colors.white,
+                                ),
+                                iconSize: controlBarHeight * 0.7,
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    )
+                  : Center(child: Text("Kamera wird aktiviert wenn Livestream ausgewählt ist")),
+            ),
+            SizedBox(height: 10),
           ],
         ),
       ),
